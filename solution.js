7adf3ccbc4c9f9c65fe9c641caf8dce9c895e652
xstartstaticx <daniel.reiller@icloud.com>
2022-06-30 12:33:05 +0200

    added solution.js

---
+++ solution.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+console.log("This is my first js file");
+
+/**
+ * Question 1 -> No, there is an error.
+ * Question 2 -> Yes, the SyntaxError for removing the opening parenthesis is: "Unexpected string."  The SyntaxError for removing the closing parenthesis is: "missing ) after argument list."
+ */
